e562b0242175ad03d5fee2d7abed4613415512c4
Harald Lang <harald.lang@in.tum.de>
2019-02-06 17:48:37 +0100

    minor

--- src/dtl/bitmap/util/bitmap_fun.hpp
+++ src/dtl/bitmap/util/bitmap_fun.hpp
@@ -9,19 +9,19 @@ template<typename _word_type>
 struct bitmap_fun {
 
   using word_type = _word_type;
-  static constexpr std::size_t block_bitlength = sizeof(word_type) * 8;
+  static constexpr std::size_t word_bitlength = sizeof(word_type) * 8;
 
   static u1 __forceinline__
   test(const word_type* b, std::size_t i) noexcept {
-    const auto block_idx = i / block_bitlength;
-    const auto bit_idx = i % block_bitlength;
+    const auto block_idx = i / word_bitlength;
+    const auto bit_idx = i % word_bitlength;
     dtl::bits::bit_test(b[block_idx], bit_idx);
   }
 
   static void __forceinline__
   set(const word_type* b, std::size_t i) noexcept {
-    const auto block_idx = i / block_bitlength;
-    const auto bit_idx = i % block_bitlength;
+    const auto block_idx = i / word_bitlength;
+    const auto bit_idx = i % word_bitlength;
     b[block_idx] ^= word_type(1) << bit_idx;
   }
 
