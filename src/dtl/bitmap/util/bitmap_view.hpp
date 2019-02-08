e12b6c2798c73d1d973fd3416e2b69b25397c165
Harald Lang <harald.lang@in.tum.de>
2019-02-08 13:34:51 +0100

    add bitmap_view

---
+++ src/dtl/bitmap/util/bitmap_view.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <vector>
+
+#include <dtl/dtl.hpp>
+#include <dtl/iterator.hpp>
+#include <dtl/bitmap/util/bitmap_fun.hpp>
+
+namespace dtl {
+
+//===----------------------------------------------------------------------===//
+template<typename _word_type>
+struct bitmap_view {
+
+  using word_type = _word_type;
+  static constexpr std::size_t word_bitlength = sizeof(word_type) * 8;
+
+  using fn = dtl::bitmap_fun<word_type>;
+  data_view<const word_type> data_;
+
+  explicit
+  bitmap_view(data_view<const word_type> data)
+      : data_(data) {
+    assert(data_.size() > 0);
+  }
+
+  bitmap_view()
+      : data_(data_view<const word_type> {nullptr, nullptr}) {
+  }
+
+  void
+  init(data_view<const word_type> data) {
+    data_ = data;
+  }
+
+  u1 __forceinline__
+  operator[](std::size_t pos) const {
+    return fn::test(data_.begin(), pos);
+  }
+
+};
+//===----------------------------------------------------------------------===//
+
+
+} // namespace dtl
+
