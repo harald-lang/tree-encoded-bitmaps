ea4af7ceb2ea36e980d6ff5857d853b8dfa8e7b6
Harald Lang <harald.lang@in.tum.de>
2019-02-26 22:35:12 +0100

    add function to generate random integer sequences

--- experiments/util/gen.hpp
+++ experiments/util/gen.hpp
@@ -5,6 +5,7 @@
 #include <dtl/dtl.hpp>
 #include <dtl/bitmap/util/random.hpp>
 #include <dtl/bitmap/util/two_state_markov_process.hpp>
+#include <dtl/bitmap/util/markov_process.hpp>
 
 //===----------------------------------------------------------------------===//
 static u1
@@ -29,7 +30,7 @@ gen_random_bitmap_markov(u64 n, $f64 f, $f64 d) {
   dtl::bitmap bs(n);
 
   // Error bounds.
-  f64 e = 0.05;
+  f64 e = 0.02;
   f64 f_min = f - f * e;
   f64 f_max = f + f * e;
   f64 d_min = d - d * e;
@@ -73,5 +74,56 @@ gen_random_bitmap_uniform(u64 n, $f64 d) {
   dtl::bitmap bs = dtl::gen_random_bitmap_uniform(n, d);
 
   return bs;
+}
+//===----------------------------------------------------------------------===//
+/// Generate a random integer sequence of length n with integers in [0, c).
+/// The parameter f refers to the clustering factor, which is the average length
+/// of runs with identical values. If f is 1, the result is uniformly
+/// distributed. - The actual f is at most 2% off.
+///
+/// Throws an exception if the parameters are invalid or a random bitmap could
+/// not be constructed after several retries.
+static std::vector<$u32>
+gen_random_integer_sequence_markov(u64 n, u32 c, $f64 f) {
+//  if (!markov_parameters_are_valid(n, f, d)) { // TODO
+//    throw std::invalid_argument("Invalid parameters for the Markov process.");
+//  }
+  assert(c < n);
+  assert(c * f < n);
+
+  markov_process mp(c, f);
+  std::vector<$u32> seq(n);
+
+  // Error bounds.
+  f64 e = 0.02;
+  f64 f_min = f - f * e;
+  f64 f_max = f + f * e;
+
+  // Retry if error is too large.
+  for ($u64 r = 0; r < 10000; ++r) {
+    std::fill(seq.begin(), seq.end(), 0);
+    // Populate the integer vector.
+    for ($u64 i = 0; i < n; i++) {
+      seq[i] = mp.next();
+    }
+    const auto f_actual = dtl::determine_clustering_factor(seq);
+    std::vector<$u32> cpy = seq;
+    std::sort(cpy.begin(), cpy.end());
+    const auto unique_cnt = std::unique(cpy.begin(), cpy.end()) - cpy.begin();
+    if (f_actual >= f_min
+        && f_actual <= f_max
+        && unique_cnt == c) {
+      return seq;
+    }
+  }
+  std::stringstream err;
+  err << "Failed: n=" << n
+      << ", c=" << c
+      << ", f=" << f
+      << std::endl;
+  std::cerr << err.str();
+  throw std::invalid_argument(
+      "Failed to construct a random integer sequence with the given parameters "
+          "after several retries.");
 }
 //===----------------------------------------------------------------------===//
