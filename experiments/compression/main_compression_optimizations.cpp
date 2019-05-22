7d892a4ebcc5655b10b22e91ca4122c16d69b26d
Harald Lang <harald.lang@in.tum.de>
2019-05-22 12:41:08 +0200

    include -o3, drop PL/RL

--- experiments/compression/main_compression_optimizations.cpp
+++ experiments/compression/main_compression_optimizations.cpp
@@ -34,12 +34,13 @@ run(const config_t& config, std::ostream& os) {
   $f64 size_teb_o0 = 0;
   $f64 size_teb_o1 = 0;
   $f64 size_teb_o2 = 0;
-  $f64 size_pl = 0;
-  $f64 size_ppl_u8 = 0;
-  $f64 size_ppl_u16 = 0;
-  $f64 size_rl = 0;
-  $f64 size_prl_u8 = 0;
-  $f64 size_prl_u16 = 0;
+  $f64 size_teb_o3 = 0;
+//  $f64 size_pl = 0;
+//  $f64 size_ppl_u8 = 0;
+//  $f64 size_ppl_u16 = 0;
+//  $f64 size_rl = 0;
+//  $f64 size_prl_u8 = 0;
+//  $f64 size_prl_u16 = 0;
 
   // # of runs
   for ($u64 r = 0; r < RUNS; r++) {
@@ -52,74 +53,77 @@ run(const config_t& config, std::ostream& os) {
     size_wah += wah.size_in_byte();
     dtl::dynamic_roaring_bitmap roaring(bm);
     size_roaring += roaring.size_in_byte();
-    dtl::teb teb_o0(bm, 0);
+    dtl::teb<0> teb_o0(bm);
     size_teb_o0 += teb_o0.size_in_byte();
-    dtl::teb teb_o1(bm, 1);
+    dtl::teb<1> teb_o1(bm);
     size_teb_o1 += teb_o1.size_in_byte();
-    dtl::teb teb_o2(bm, 2);
-    teb_o2.run_optimize();
+    dtl::teb<2> teb_o2(bm);
     size_teb_o2 += teb_o2.size_in_byte();
-    dtl::position_list<$u32> pl(bm);
-    size_pl += pl.size_in_byte();
-    dtl::partitioned_position_list<$u32, $u8> ppl_u8(bm);
-    size_ppl_u8 += ppl_u8.size_in_byte();
-    dtl::partitioned_position_list<$u32, $u16> ppl_u16(bm);
-    size_ppl_u16 += ppl_u16.size_in_byte();
-    dtl::range_list<$u32> rl(bm);
-    size_rl += rl.size_in_byte();
-    dtl::partitioned_range_list<$u32, $u8> prl_u8(bm);
-    size_prl_u8 += prl_u8.size_in_byte();
-    dtl::partitioned_range_list<$u32, $u16> prl_u16(bm);
-    size_prl_u16 += prl_u16.size_in_byte();
+    dtl::teb<3> teb_o3(bm);
+    size_teb_o3 += teb_o3.size_in_byte();
+//    dtl::position_list<$u32> pl(bm);
+//    size_pl += pl.size_in_byte();
+//    dtl::partitioned_position_list<$u32, $u8> ppl_u8(bm);
+//    size_ppl_u8 += ppl_u8.size_in_byte();
+//    dtl::partitioned_position_list<$u32, $u16> ppl_u16(bm);
+//    size_ppl_u16 += ppl_u16.size_in_byte();
+//    dtl::range_list<$u32> rl(bm);
+//    size_rl += rl.size_in_byte();
+//    dtl::partitioned_range_list<$u32, $u8> prl_u8(bm);
+//    size_prl_u8 += prl_u8.size_in_byte();
+//    dtl::partitioned_range_list<$u32, $u16> prl_u16(bm);
+//    size_prl_u16 += prl_u16.size_in_byte();
 
     // Validation
     {
-      if (bm != wah.to_bitset()) {
+      if (bm != dtl::to_bitmap_using_iterator(wah)) {
         std::cerr << "Validation failed. (WAH)" << std::endl;
         std::exit(1);
       }
-      if (bm != roaring.to_bitset()) {
+      if (bm != dtl::to_bitmap_using_iterator(roaring)) {
         std::cerr << "Validation failed. (Roaring)" << std::endl;
         std::exit(1);
       }
-      if (bm != teb_o0.to_bitset()) {
+      if (bm != dtl::to_bitmap_using_iterator(teb_o0)) {
         std::cerr << "Validation failed. (TEBo0)" << std::endl;
         std::exit(1);
       }
-      if (bm != teb_o1.to_bitset()) {
+      if (bm != dtl::to_bitmap_using_iterator(teb_o1)) {
         std::cerr << "Validation failed. (TEBo1)" << std::endl;
         std::exit(1);
       }
-      if (bm != teb_o2.to_bitset()) {
+      if (bm != dtl::to_bitmap_using_iterator(teb_o2)) {
         std::cerr << "Validation failed. (TEBo2)" << std::endl;
-        std::cerr << "expected: " << bm << std::endl;
-        std::cerr << "actual:   " << teb_o2.to_bitset() << std::endl;
         std::exit(1);
       }
-      if (bm != pl.to_bitset()) {
-        std::cerr << "Validation failed. (PL)" << std::endl;
+      if (bm != dtl::to_bitmap_using_iterator(teb_o3)) {
+        std::cerr << "Validation failed. (TEBo3)" << std::endl;
         std::exit(1);
       }
-      if (bm != ppl_u8.to_bitset()) {
-        std::cerr << "Validation failed. (PPLu8)" << std::endl;
-        std::exit(1);
-      }
-      if (bm != ppl_u16.to_bitset()) {
-        std::cerr << "Validation failed. (PPLu16)" << std::endl;
-        std::exit(1);
-      }
-      if (bm != rl.to_bitset()) {
-        std::cerr << "Validation failed. (RL)" << std::endl;
-        std::exit(1);
-      }
-      if (bm != prl_u8.to_bitset()) {
-        std::cerr << "Validation failed. (PRLu8)" << std::endl;
-        std::exit(1);
-      }
-      if (bm != prl_u16.to_bitset()) {
-        std::cerr << "Validation failed. (PRLu16)" << std::endl;
-        std::exit(1);
-      }
+//      if (bm != pl.to_bitset()) {
+//        std::cerr << "Validation failed. (PL)" << std::endl;
+//        std::exit(1);
+//      }
+//      if (bm != ppl_u8.to_bitset()) {
+//        std::cerr << "Validation failed. (PPLu8)" << std::endl;
+//        std::exit(1);
+//      }
+//      if (bm != ppl_u16.to_bitset()) {
+//        std::cerr << "Validation failed. (PPLu16)" << std::endl;
+//        std::exit(1);
+//      }
+//      if (bm != rl.to_bitset()) {
+//        std::cerr << "Validation failed. (RL)" << std::endl;
+//        std::exit(1);
+//      }
+//      if (bm != prl_u8.to_bitset()) {
+//        std::cerr << "Validation failed. (PRLu8)" << std::endl;
+//        std::exit(1);
+//      }
+//      if (bm != prl_u16.to_bitset()) {
+//        std::cerr << "Validation failed. (PRLu16)" << std::endl;
+//        std::exit(1);
+//      }
     }
   }
 
@@ -130,25 +134,28 @@ run(const config_t& config, std::ostream& os) {
       << "," << (size_teb_o0 / RUNS) / 1024.0
       << "," << (size_teb_o1 / RUNS) / 1024.0
       << "," << (size_teb_o2 / RUNS) / 1024.0
+      << "," << (size_teb_o3 / RUNS) / 1024.0
       << "," << (size_roaring / RUNS) / 1024.0
       << "," << (size_wah / RUNS) / 1024.0
-      << "," << (size_pl / RUNS) / 1024.0
-      << "," << (size_ppl_u8 / RUNS) / 1024.0
-      << "," << (size_ppl_u16 / RUNS) / 1024.0
-      << "," << (size_rl / RUNS) / 1024.0
-      << "," << (size_prl_u8 / RUNS) / 1024.0
-      << "," << (size_prl_u16 / RUNS) / 1024.0
+//      << "," << (size_pl / RUNS) / 1024.0
+//      << "," << (size_ppl_u8 / RUNS) / 1024.0
+//      << "," << (size_ppl_u16 / RUNS) / 1024.0
+//      << "," << (size_rl / RUNS) / 1024.0
+//      << "," << (size_prl_u8 / RUNS) / 1024.0
+//      << "," << (size_prl_u16 / RUNS) / 1024.0
       << std::endl;
   os << out.str();
 }
 
 $i32 main() {
 
-//  u64 N = 1ull << 20;
   std::vector<$u64> n_s {
-      1ull << 10, 1ull << 12, 1ull << 14, 1ull << 16, 1ull << 18, 1ull << 20 };
-//  std::vector<$f64> clustering_factors { 8 };
-  std::vector<$f64> clustering_factors { 1, 2, 4, 8, 16, 32, 64, 128 };
+      1ull << 20 };
+  std::vector<$f64> clustering_factors { 8 };
+//  std::vector<$u64> n_s {
+//      1ull << 10, 1ull << 12, 1ull << 14, 1ull << 16, 1ull << 18, 1ull << 20 };
+//  std::vector<$f64> clustering_factors { 1, 2, 4, 8, 16, 32, 64, 128 };
+
   std::vector<$f64> bit_densities { 0.01 };
   for ($f64 d = 5; d <= 85; d += 5) {
     bit_densities.push_back(d/100);
