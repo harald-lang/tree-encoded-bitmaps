76f979ea11d08dc1f2337c2b526ee028db00f26d
Harald Lang <harald.lang@in.tum.de>
2019-09-10 22:46:56 +0200

    add function to fetch consecutive bits

--- src/dtl/bitmap/util/plain_bitmap.hpp
+++ src/dtl/bitmap/util/plain_bitmap.hpp
@@ -43,7 +43,7 @@ public:
   u1 __teb_inline__
   operator[](std::size_t i) const {
     assert((i / word_bitlength) < bitmap_.size());
-    assert(i < n_);
+    assert(i <= n_);
     return bitmap_fun<word_type>::test(bitmap_.data(), i);
   }
 
@@ -51,7 +51,7 @@ public:
   void __teb_inline__
   set(std::size_t i, u1 val) {
     assert((i / word_bitlength) < bitmap_.size());
-    assert(i < n_);
+    assert(i <= n_);
     bitmap_fun<word_type>::set(bitmap_.data(), i, val);
   }
 
@@ -59,7 +59,7 @@ public:
   void __teb_inline__
   set(std::size_t i) {
     assert((i / word_bitlength) < bitmap_.size());
-    assert(i < n_);
+    assert(i <= n_);
     bitmap_fun<word_type>::set(bitmap_.data(), i);
   }
 
@@ -67,8 +67,8 @@ public:
   void __teb_inline__
   set(std::size_t b, std::size_t e) {
     assert((b / word_bitlength) < bitmap_.size());
-    assert(b < n_);
-    assert(e < n_);
+    assert(b <= n_);
+    assert(e <= n_);
     assert(b < e);
     bitmap_fun<word_type>::set(bitmap_.data(), b, e);
   }
@@ -77,7 +77,7 @@ public:
   void __teb_inline__
   clear(std::size_t i) {
     assert((i / word_bitlength) < bitmap_.size());
-    assert(i < n_);
+    assert(i <= n_);
     bitmap_fun<word_type>::clear(bitmap_.data(), i);
   }
 
@@ -85,8 +85,8 @@ public:
   void __teb_inline__
   clear(std::size_t b, std::size_t e) {
     assert((b / word_bitlength) < bitmap_.size());
-    assert(b < n_);
-    assert(e < n_);
+    assert(b <= n_);
+    assert(e <= n_);
     assert(b < e);
     bitmap_fun<word_type>::clear(bitmap_.data(), b, e);
   }
@@ -94,6 +94,19 @@ public:
   __teb_inline__ word_type*
   data() {
     return bitmap_.data();
+  }
+
+  /// Fetch up to size(word_type)*8 consecutive bits from the range [b,e).
+  /// Note: The behavior is undefined when the range spans more bits than fit in
+  /// a single word.
+  word_type __teb_inline__
+  fetch_bits(std::size_t b, std::size_t e) const {
+    assert((b / word_bitlength) < bitmap_.size());
+    assert(b <= n_);
+    assert(e <= n_);
+    assert(b < e);
+    assert((e - b) <= (sizeof(word_type) * 8));
+    return bitmap_fun<word_type>::fetch_bits(bitmap_.data(), b, e);
   }
 
 };
