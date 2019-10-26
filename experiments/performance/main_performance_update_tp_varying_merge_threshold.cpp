717f0c5fccecc3253f05361399140d6d28175ef9
Harald Lang <harald.lang@in.tum.de>
2019-10-26 22:14:00 +0200

    add experiment that measures the update throughput with varying merge thresholds

---
+++ experiments/performance/main_performance_update_tp_varying_merge_threshold.cpp
@@ -0,0 +1,335 @@
+#include "common.hpp"
+#include "experiments/util/bitmap_db.hpp"
+#include "experiments/util/bitmap_types.hpp"
+#include "experiments/util/differential_bitmap_types.hpp"
+#include "experiments/util/gen.hpp"
+#include "experiments/util/params.hpp"
+#include "experiments/util/prep_data.hpp"
+#include "experiments/util/prep_updates.hpp"
+
+#include <dtl/dtl.hpp>
+
+#include <dtl/bitmap/bitwise_operations.hpp>
+#include <dtl/bitmap/diff/diff.hpp>
+#include <dtl/bitmap/diff/merge.hpp>
+#include <dtl/bitmap/diff/merge_teb.hpp>
+#include <iostream>
+#include <random>
+#include <vector>
+//===----------------------------------------------------------------------===//
+// Experiment: Measures the update performance for varying number of merge
+//             thresholds.
+//===----------------------------------------------------------------------===//
+struct task {
+//  diff_bitmap_t bitmap_type; // under test
+//  diff_merge_t merge_type;
+  $u64 bitmap_id_a;
+  $u64 bitmap_id_b;
+  $f64 update_threshold;
+  $f64 merge_threshold;
+};
+//===----------------------------------------------------------------------===//
+struct benchmark_results {
+  std::size_t n;
+  std::string type_name;
+  std::string type_info_before_updates;
+  std::string type_info_after_updates;
+  std::string merge_type_name;
+  std::size_t encoded_size_before_updates;
+  std::size_t encoded_size_after_updates;
+  std::size_t update_cnt;
+  std::size_t merge_cnt;
+  $u64 runtime_nanos = 0;
+  $u64 runtime_cycles = 0;
+  $u64 runtime_update_nanos = 0;
+  $u64 runtime_update_cycles = 0;
+  $u64 runtime_merge_nanos = 0;
+  $u64 runtime_merge_cycles = 0;
+  // Don't care.
+  std::size_t checksum = 0;
+};
+//===----------------------------------------------------------------------===//
+template<typename T, typename M>
+benchmark_results __attribute__((noinline))
+run_benchmark(
+    const dtl::bitmap b,
+    const std::vector<update_entry>& updates,
+    $u64 pending_updates_threshold) {
+
+  const std::size_t total_update_cnt = updates.size();
+  pending_updates_threshold = std::min(pending_updates_threshold, updates.size());
+
+  const auto duration_nanos = 1250e6;
+  const std::size_t MIN_REPS = 5;
+
+  benchmark_results res;
+  res.n = b.size();
+  {
+    T enc(b);
+    res.encoded_size_before_updates = enc.size_in_byte();
+    res.type_name = enc.name();
+    res.type_info_before_updates = enc.info();
+    res.merge_type_name = M::name();
+  }
+
+  // The actual measurement.
+  res.runtime_nanos = 0;
+  res.runtime_cycles = 0;
+  res.runtime_update_nanos = 0;
+  res.runtime_update_cycles = 0;
+  res.runtime_merge_nanos = 0;
+  res.runtime_merge_cycles = 0;
+
+  // The repetition loop.
+  const auto nanos_begin = now_nanos();
+  const auto nanos_term = nanos_begin + duration_nanos;
+  std::size_t rep_cntr = 0;
+  std::size_t checksum = 0;
+  while (now_nanos() < nanos_term || rep_cntr < MIN_REPS) {
+    ++rep_cntr;
+
+    // Encode the bitmap.
+    T enc(b);
+
+    std::size_t applied_update_cnt = 0;
+    std::size_t applied_merge_cnt = 0;
+    while (applied_update_cnt < total_update_cnt) {
+
+      // The number of updates before the next merge happens.
+      const std::size_t batch_size =
+          std::min(pending_updates_threshold, total_update_cnt - applied_update_cnt);
+
+      const std::size_t batch_begin = applied_update_cnt;
+      const std::size_t batch_end = applied_update_cnt + batch_size;
+
+      const auto nanos_begin_updates = now_nanos();
+      const auto cycles_begin_updates = _rdtsc();
+
+      // Apply the updates.
+      for (std::size_t i = batch_begin; i < batch_end; ++i) {
+        const auto& u = updates[i];
+        enc.set(u.pos, u.value);
+      }
+
+      const auto cycles_end_updates = _rdtsc();
+      const auto nanos_end_updates = now_nanos();
+
+      res.runtime_update_nanos += nanos_end_updates - nanos_begin_updates;
+      res.runtime_update_cycles += cycles_end_updates - cycles_begin_updates;
+      applied_update_cnt += batch_size;
+
+      if (batch_size == pending_updates_threshold) {
+        // Merge the updates.
+        const auto nanos_begin_merge = now_nanos();
+        const auto cycles_begin_merge = _rdtsc();
+
+        enc.template merge<M>();
+
+        const auto cycles_end_merge = _rdtsc();
+        const auto nanos_end_merge = now_nanos();
+
+        res.runtime_merge_nanos += nanos_end_merge - nanos_begin_merge;
+        res.runtime_merge_cycles += cycles_end_merge - cycles_begin_merge;
+        ++applied_merge_cnt;
+      }
+    }
+    res.encoded_size_after_updates = enc.size_in_byte();
+    checksum += res.encoded_size_after_updates;
+    res.type_info_after_updates = enc.info();
+    res.update_cnt = applied_update_cnt;
+    res.merge_cnt = applied_merge_cnt;
+  }
+  res.checksum = checksum;
+
+  res.runtime_update_nanos /= rep_cntr;
+  res.runtime_update_cycles /= rep_cntr;
+  res.runtime_merge_nanos /= rep_cntr;
+  res.runtime_merge_cycles /= rep_cntr;
+
+  res.runtime_nanos = res.runtime_update_nanos + res.runtime_merge_nanos;
+  res.runtime_cycles = res.runtime_update_cycles + res.runtime_merge_cycles;
+
+  return res;
+}
+//===----------------------------------------------------------------------===//
+template<typename T, typename M>
+void
+do_measurement(task& t) {
+  // The bitmap that is used as starting point.
+  const auto bm_a = db.load_bitmap(t.bitmap_id_a);
+  const auto bm_a_density = dtl::determine_bit_density(bm_a);
+  const auto bm_a_clustering_factor = dtl::determine_clustering_factor(bm_a);
+  // The second bitmap determines the updates to perform.
+  const auto bm_b = db.load_bitmap(t.bitmap_id_b);
+  const auto bm_b_density = dtl::determine_bit_density(bm_b);
+  const auto bm_b_clustering_factor = dtl::determine_clustering_factor(bm_b);
+
+  assert(bm_a.size() == bm_b.size());
+
+  // Prepare the updates. The updates are clustered, so that the characteristics
+  // of the bitmap remains roughly the same during modifications.
+  auto range_updates = prepare_range_updates(bm_a, bm_b);
+
+  // Shuffle the update order.
+  std::mt19937 gen(42); // for reproducible results, same order among all benchmarks
+  std::shuffle(range_updates.begin(), range_updates.end(), gen);
+
+  // The maximum number of updates to perform.
+  const std::size_t max_updates = t.update_threshold;
+
+  std::vector<update_entry> updates;
+  updates.reserve(bm_b.count());
+  for (auto& range : range_updates) {
+    for (std::size_t i = range.pos; i < range.pos + range.length; ++i) {
+      if (updates.size() < max_updates) {
+        updates.emplace_back(i, range.value);
+      }
+    }
+  }
+
+  auto res = run_benchmark<T, M>(bm_a, updates, t.merge_threshold);
+
+  std::string type_info_before_updates = res.type_info_before_updates;
+  boost::replace_all(type_info_before_updates, "\"", "\"\""); // Escape JSON for CSV output.
+  std::string type_info_after_updates = res.type_info_after_updates;
+  boost::replace_all(type_info_after_updates, "\"", "\"\""); // Escape JSON for CSV output.
+
+  std::cout << RUN_ID
+      << ",\"" << BUILD_ID << "\""
+      << "," << res.n
+      << "," << "\"" << res.type_name << "\""
+      << "," << "\"" << res.merge_type_name << "\""
+      << "," << t.bitmap_id_a
+      << "," << t.bitmap_id_b
+      << "," << (res.runtime_nanos / res.update_cnt)
+      << "," << (res.runtime_nanos / res.merge_cnt)
+      << "," << std::min(std::size_t(t.update_threshold), updates.size())
+      << "," << t.merge_threshold
+      << "," << res.update_cnt
+      << "," << res.merge_cnt
+      << "," << res.runtime_nanos
+//      << "," << res.runtime_cycles
+      << "," << res.runtime_update_nanos
+//      << "," << res.runtime_update_cycles
+      << "," << res.runtime_merge_nanos
+//      << "," << res.runtime_merge_cycles
+      << "," << dtl::determine_bit_density(bm_a)
+      << "," << dtl::determine_clustering_factor(bm_a)
+      << "," << dtl::determine_bit_density(bm_b)
+      << "," << dtl::determine_clustering_factor(bm_b)
+      << "," << res.encoded_size_before_updates
+      << "," << res.encoded_size_after_updates
+      << "," << "\"" << type_info_before_updates << "\""
+      << "," << "\"" << type_info_after_updates << "\""
+      << "," << res.checksum
+      << std::endl;
+}
+//===----------------------------------------------------------------------===//
+template<diff_bitmap_t T, diff_merge_t M>
+void
+do_measurement(task& t) {
+  for (std::size_t mt : {1000, 2500, 5000, 7500, 10000, 12500, 15000, 17500, 20000, 22500, 25000, 27500, 30000, 32500, 35000, 37500, 40000, 42500, 45000, 47500, 50000}){
+//  for (std::size_t mt : {1000, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000}){
+    t.merge_threshold = mt;
+    do_measurement<
+        typename diff_type_of<T>::type,
+        typename diff_merge_type_of<T, M>::type>(t);
+  }
+}
+//===----------------------------------------------------------------------===//
+$i32 main() {
+  // Prepare benchmark settings.
+  u64 n_min = 1ull << 20;
+  u64 n_max = 1ull << 20;
+
+  // Use the same setting as with the compression skyline.
+  std::cerr << "run_id=" << RUN_ID << std::endl;
+  std::cerr << "build_id=" << BUILD_ID << std::endl;
+  std::vector<$f64> clustering_factors;
+  clustering_factors.push_back(8.0);
+
+  std::vector<$f64> bit_densities;
+  bit_densities.push_back(0.1);
+//  bit_densities.push_back(0.25);
+
+  std::vector<$u64> n_values;
+  for ($u64 n = n_min; n <= n_max; n <<= 1) {
+    n_values.push_back(n);
+  }
+
+  if (GEN_DATA) {
+    std::vector<params_markov> params;
+    for (auto f : clustering_factors) {
+      for (auto d : bit_densities) {
+        for (auto n : n_values) {
+          if (!markov_parameters_are_valid(n, f, d)) continue;
+          params_markov p;
+          p.n = n;
+          p.clustering_factor = f;
+          p.density = d;
+          params.push_back(p);
+        }
+      }
+    }
+    prep_data(params, RUNS, db);
+    std::exit(0);
+  }
+  else {
+    if (db.empty()) {
+      std::cerr << "Bitmap database is empty. Use GEN_DATA=1 to populate the "
+                   "database."
+                << std::endl;
+      std::exit(1);
+    }
+  }
+
+  for (auto f : clustering_factors) {
+    for (auto d : bit_densities) {
+      for (auto n : n_values) {
+        if (!markov_parameters_are_valid(n, f, d)) continue;
+
+        auto bitmap_ids = db.find_bitmaps(n, f, d);
+        if (bitmap_ids.empty()) {
+          continue;
+        }
+        if (bitmap_ids.size() < RUNS) {
+          std::cerr << "There are only " << bitmap_ids.size() << " prepared "
+                    << "bitmaps for the parameters n=" << n << ", f=" << f
+                    << ", d=" << d << ", but " << RUNS << " are required."
+                    << std::endl;
+        }
+        if (bitmap_ids.size() >= 2) {
+          task t;
+          t.bitmap_id_a = bitmap_ids[0];
+          t.bitmap_id_b = bitmap_ids[1];
+          t.update_threshold = 100000;
+//          do_measurement<diff_bitmap_t::part_teb,        diff_merge_t::no>(t); // DON'T! VERY SLOW
+//          do_measurement<diff_bitmap_t::part_wah,        diff_merge_t::no>(t); // DON'T! VERY SLOW
+          do_measurement<diff_bitmap_t::part_diff_teb,   diff_merge_t::naive>(t);
+          do_measurement<diff_bitmap_t::part_diff_teb,   diff_merge_t::naive_iter>(t);
+          do_measurement<diff_bitmap_t::part_diff_teb,   diff_merge_t::tree>(t);
+
+          do_measurement<diff_bitmap_t::part_diff_wah,   diff_merge_t::naive>(t);
+          do_measurement<diff_bitmap_t::part_diff_wah,   diff_merge_t::naive_iter>(t);
+
+          do_measurement<diff_bitmap_t::roaring_roaring, diff_merge_t::naive>(t);
+          do_measurement<diff_bitmap_t::roaring_roaring, diff_merge_t::naive_iter>(t);
+          do_measurement<diff_bitmap_t::roaring_roaring, diff_merge_t::inplace>(t);
+
+          do_measurement<diff_bitmap_t::teb_roaring,     diff_merge_t::naive>(t);
+          do_measurement<diff_bitmap_t::teb_roaring,     diff_merge_t::naive_iter>(t);
+          do_measurement<diff_bitmap_t::teb_roaring,     diff_merge_t::tree>(t);
+
+          do_measurement<diff_bitmap_t::wah_roaring,     diff_merge_t::naive>(t);
+          do_measurement<diff_bitmap_t::wah_roaring,     diff_merge_t::naive_iter>(t);
+
+          // DON'T use WAH as diff structure! SLOW
+//          do_measurement<diff_bitmap_t::roaring_wah,     diff_merge_t::naive>(t);
+//          do_measurement<diff_bitmap_t::teb_wah,         diff_merge_t::naive>(t);
+//          do_measurement<diff_bitmap_t::wah_wah,         diff_merge_t::naive>(t);
+        }
+      }
+    }
+  }
+}
+//===----------------------------------------------------------------------===//
--- experiments/util/differential_bitmap_types.hpp
+++ experiments/util/differential_bitmap_types.hpp
@@ -1,8 +1,12 @@
 #pragma once
 //===----------------------------------------------------------------------===//
 #include <dtl/bitmap/diff/diff.hpp>
+#include <dtl/bitmap/diff/merge.hpp>
+#include <dtl/bitmap/diff/merge_teb.hpp>
 #include <dtl/bitmap/dynamic_roaring_bitmap.hpp>
 #include <dtl/bitmap/dynamic_wah.hpp>
+#include <dtl/bitmap/part/part_updirect.hpp>
+#include <dtl/bitmap/part/part_upforward.hpp>
 #include <dtl/bitmap/teb_wrapper.hpp>
 //===----------------------------------------------------------------------===//
 #define __DIFF_NAME_ROARING_WAH roaring_wah
@@ -23,7 +27,20 @@
 #define __DIFF_NAME_WAH_ROARING wah_roaring
 #define __DIFF_TYPE_WAH_ROARING dtl::diff<dtl::dynamic_wah32, dtl::dynamic_roaring_bitmap>
 //===----------------------------------------------------------------------===//
+using partitioned_teb = dtl::part_updirect<dtl::teb_wrapper, 1ull << 16>;
+using partitioned_teb_diff_structure = dtl::dynamic_roaring_bitmap;
+using partitioned_differential_teb =
+    dtl::part_upforward<dtl::diff<dtl::teb_wrapper,partitioned_teb_diff_structure>,
+    1ull << 16>;
 
+using partitioned_wah = dtl::part_updirect<dtl::dynamic_wah32, 1ull << 16>;
+using partitioned_wah_diff_structure = dtl::dynamic_roaring_bitmap;
+//using partitioned_wah_diff_structure = dtl::dynamic_wah32; // WAH disqualifies as diff structure, in general
+using partitioned_differential_wah =
+dtl::part_upforward<dtl::diff<dtl::dynamic_wah32,partitioned_wah_diff_structure>,
+    1ull << 16>;
+//===----------------------------------------------------------------------===//
+// FIXME misnormer ... updatable bitmap types
 enum class diff_bitmap_t {
   __DIFF_NAME_ROARING_WAH,
   __DIFF_NAME_ROARING_ROARING,
@@ -31,11 +48,41 @@ enum class diff_bitmap_t {
   __DIFF_NAME_TEB_ROARING,
   __DIFF_NAME_WAH_WAH,
   __DIFF_NAME_WAH_ROARING,
+  roaring,
+  part_teb, // a partitioned TEB which supports direct updates
+  part_diff_teb, // a partitioned TEB where each partition has a diff structure
+  part_wah, // a partitioned WAH which supports direct updates
+  part_diff_wah, // a partitioned WAH where each partition has a diff structure
 };
 //===----------------------------------------------------------------------===//
 template<diff_bitmap_t B>
 struct diff_type_of {
   using type = void;
+};
+
+template<>
+struct diff_type_of<diff_bitmap_t::roaring> {
+  using type = dtl::dynamic_roaring_bitmap;
+};
+
+template<>
+struct diff_type_of<diff_bitmap_t::part_teb> {
+  using type = partitioned_teb;
+};
+
+template<>
+struct diff_type_of<diff_bitmap_t::part_diff_teb> {
+  using type = partitioned_differential_teb;
+};
+
+template<>
+struct diff_type_of<diff_bitmap_t::part_wah> {
+  using type = partitioned_wah;
+};
+
+template<>
+struct diff_type_of<diff_bitmap_t::part_diff_wah> {
+  using type = partitioned_differential_wah;
 };
 
 #define __GENERATE(NAME, TYPE)               \
@@ -50,9 +97,95 @@ __GENERATE(__DIFF_NAME_TEB_WAH, __DIFF_TYPE_TEB_WAH)
 __GENERATE(__DIFF_NAME_TEB_ROARING, __DIFF_TYPE_TEB_ROARING)
 __GENERATE(__DIFF_NAME_WAH_WAH, __DIFF_TYPE_WAH_WAH)
 __GENERATE(__DIFF_NAME_WAH_ROARING, __DIFF_TYPE_WAH_ROARING)
+#undef __GENERATE
+//===----------------------------------------------------------------------===//
+enum class diff_merge_t {
+  naive,
+  naive_iter,
+  inplace,
+  tree,
+  no,
+  forward,
+};
+//===----------------------------------------------------------------------===//
+template<diff_bitmap_t B, diff_merge_t M>
+struct diff_merge_type_of {
+  using type = void;
+};
+
+#define __GENERATE(BNAME, MNAME, MTYPE)                                  \
+  template<>                                                             \
+  struct diff_merge_type_of<diff_bitmap_t::BNAME, diff_merge_t::MNAME> { \
+    using type = MTYPE<                                                  \
+        diff_type_of<diff_bitmap_t::BNAME>::type::bitmap_type,           \
+        diff_type_of<diff_bitmap_t::BNAME>::type::diff_type>;            \
+  };
+
+__GENERATE(__DIFF_NAME_ROARING_ROARING, naive, dtl::merge_naive)
+__GENERATE(__DIFF_NAME_ROARING_ROARING, naive_iter, dtl::merge_naive_iter)
+__GENERATE(__DIFF_NAME_ROARING_ROARING, inplace, dtl::merge_inplace)
+
+__GENERATE(__DIFF_NAME_ROARING_WAH, naive, dtl::merge_naive)
+__GENERATE(__DIFF_NAME_ROARING_WAH, naive_iter, dtl::merge_naive_iter)
+
+__GENERATE(__DIFF_NAME_TEB_ROARING, naive, dtl::merge_naive)
+__GENERATE(__DIFF_NAME_TEB_ROARING, naive_iter, dtl::merge_naive_iter)
+__GENERATE(__DIFF_NAME_TEB_ROARING, tree, dtl::merge_tree)
+
+__GENERATE(__DIFF_NAME_TEB_WAH, naive, dtl::merge_naive)
+__GENERATE(__DIFF_NAME_TEB_WAH, naive_iter, dtl::merge_naive_iter)
+__GENERATE(__DIFF_NAME_TEB_WAH, tree, dtl::merge_tree)
+
+__GENERATE(__DIFF_NAME_WAH_ROARING, naive, dtl::merge_naive)
+__GENERATE(__DIFF_NAME_WAH_ROARING, naive_iter, dtl::merge_naive_iter)
+
+__GENERATE(__DIFF_NAME_WAH_WAH, naive, dtl::merge_naive)
+__GENERATE(__DIFF_NAME_WAH_WAH, naive_iter, dtl::merge_naive_iter)
+__GENERATE(__DIFF_NAME_WAH_WAH, inplace, dtl::merge_inplace)
 
 #undef __GENERATE
 
+// Partitioned TEB.
+template<>
+struct diff_merge_type_of<diff_bitmap_t::part_teb, diff_merge_t::no> {
+  using type = dtl::merge_not<partitioned_teb, void>; // no merge
+};
+
+// Partitioned WAH.
+template<>
+struct diff_merge_type_of<diff_bitmap_t::part_wah, diff_merge_t::no> {
+  using type = dtl::merge_not<partitioned_wah, void>; // no merge
+};
+
+// Partitioned/differential TEB.
+template<>
+struct diff_merge_type_of<diff_bitmap_t::part_diff_teb, diff_merge_t::naive> {
+  // Merge is handled by the bitmap itself.
+  using type = dtl::merge_naive<dtl::teb_wrapper,partitioned_teb_diff_structure>;
+};
+template<>
+struct diff_merge_type_of<diff_bitmap_t::part_diff_teb, diff_merge_t::naive_iter> {
+  // Merge is handled by the bitmap itself.
+  using type = dtl::merge_naive_iter<dtl::teb_wrapper,partitioned_teb_diff_structure>;
+};
+template<>
+struct diff_merge_type_of<diff_bitmap_t::part_diff_teb, diff_merge_t::tree> {
+  // Merge is handled by the bitmap itself.
+  using type = dtl::merge_tree<dtl::teb_wrapper,partitioned_teb_diff_structure>;
+};
+
+// Partitioned/differential WAH.
+template<>
+struct diff_merge_type_of<diff_bitmap_t::part_diff_wah, diff_merge_t::naive> {
+  // Merge is handled by the bitmap itself.
+  using type = dtl::merge_naive<dtl::dynamic_wah32,partitioned_wah_diff_structure>;
+};
+template<>
+struct diff_merge_type_of<diff_bitmap_t::part_diff_wah, diff_merge_t::naive_iter> {
+  // Merge is handled by the bitmap itself.
+  using type = dtl::merge_naive_iter<dtl::dynamic_wah32,partitioned_wah_diff_structure>;
+};
+//===----------------------------------------------------------------------===//
 #undef __DIFF_NAME_ROARING_WAH
 #undef __DIFF_NAME_ROARING_ROARING
 #undef __DIFF_NAME_TEB_WAH
